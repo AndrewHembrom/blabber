2f9b9b1127d1b4957424511e456a7e989344b020
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2025-06-18 21:22:58 +0530

    socket connection and disconnection and testing with test.html in client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import http from "http";
+import { Server, Socket } from "socket.io"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
 
 const port = 3000;
 
@@ -12,6 +21,14 @@ app.get("/", (req:Request , res:Response) => {
     res.send("Server is working");
 })
 
-app.listen(port, () => {
+io.on("connection", (socket: Socket) => {
+    console.log(`User connected: ${socket.id}`);
+    
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
